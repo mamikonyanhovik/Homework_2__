7308898849d736061679beb6b699e6dad7c81bb7
mamikonyanhovik <58630430+mamikonyanhovik@users.noreply.github.com>
2019-12-17 23:37:32 +0400

    Add files via upload

---
+++ Homework_02--ex_05.js
@@ -0,0 +1,8 @@
+var number = 10;
+var sum = null;
+for (var i=1; i<=number; i++) {
+  if (i===8)
+  continue;
+sum += i
+};
+console.log(sum);
---
+++ Homework_02--ex_08.js
@@ -0,0 +1,27 @@
+for (var i=1; i<=12; i++) {
+  if (i===1)
+  console.log(i + ' = January');
+  else if (i===2)
+  console.log(i + ' = February');
+  else if (i===3)
+  console.log(i + ' = March');
+  else if (i===4)
+  console.log(i + ' = April');
+  else if (i===5)
+  console.log(i + ' = May');
+  else if (i===6)
+  console.log(i + ' = June');
+  else if (i===7)
+  console.log(i + ' = July');
+  else if (i===8)
+  console.log(i + ' = August');
+  else if (i===9)
+  console.log(i + ' = September');
+  else if (i===10)
+  console.log(i + ' = October');
+  else if (i===11)
+  console.log(i + ' = November');
+  else if (i===12)
+  console.log(i + ' = December');
+
+}
